afe8bf32510bf68005b6b853f33603f499412aaf
Dylan Tran <dylanbt928@gmail.com>
2025-12-08 15:50:51 -0800

    reformat framebuffer.h

--- kernel/include/drivers/framebuffer.h
+++ kernel/include/drivers/framebuffer.h
@@ -1,17 +1,18 @@
 #ifndef FRAMEBUFFER_H
 #define FRAMEBUFFER_H
 
-#include <stdint.h>
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-typedef struct {
-    void *address;              
-    uint64_t width;             
-    uint64_t height;            
-    uint64_t pitch;             
-    uint16_t bits_per_pixel;               
-    uint8_t bytes_per_pixel;     
+typedef struct
+{
+    void* address;
+    uint64_t width;
+    uint64_t height;
+    uint64_t pitch;
+    uint16_t bits_per_pixel;
+    uint8_t bytes_per_pixel;
 } framebuffer_t;
 
 bool framebuffer_init(void);
@@ -20,6 +21,6 @@ void fb_put_pixel(int x, int y, uint32_t color);
 void fb_clear(uint32_t color);
 void fb_fill_rect(uint64_t x, uint64_t y, uint64_t width, uint64_t height, uint32_t color);
 void fb_draw_char(char c, int x, int y, uint32_t fg_color, uint32_t bg_color);
-void fb_draw_string(const char *str, int x, int y, uint32_t fg_color, uint32_t bg_color);
+void fb_draw_string(const char* str, int x, int y, uint32_t fg_color, uint32_t bg_color);
 
 #endif
