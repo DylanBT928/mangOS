e536d8658fb5c871945776e080a2225342643bee
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-09 22:49:25 -0500

    set up framebuffer_init function

--- kernel/src/drivers/framebuffer.c
+++ kernel/src/drivers/framebuffer.c
@@ -1,7 +1,66 @@
 #include "drivers/framebuffer.h"
+#include "drivers/serial.h"
+#include <kernel.h>
+#include <limine.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// DO NOT CHANGE -- PLEASE CONSULT BEFORE CHANGING ------------------------------------
+
+// Sets the base revision to version number 4 to insure compatibility between kernal
+// and the bootloader. Will help to overcome shortcomings of older boot protocals and
+// provides good access to 64_bit long mode and other modern niceties
+// DO NOT CHANGE BASE REVISION: ALL OTHER VERSIONS ARE DEPRECATED OUR CODE WILL
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(4);
+
+// Most Limine requests should be made volatile so the compiler does not optimize them
+// away. They should also be accessed at least once or marked with "used" attribute
+// The framebuffer is a portion of ram that stores image data for video display, we
+// need this for any kind of image to be accessed
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+    .revision = 0
+};
+
+// Here we set the start and end markers for the Limine requests
+// these can be moved anywhere, but just dont they work right now
+__attribute__((used, section(".limine_requests_start")))
+static volatile uint64_t limine__requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
+__attribute__((used, section(".limine_requests_end")))
+static volatile uint64_t limine__requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
+
+// Halt and catch fire
+// This function leads to a complete system crash, calling inline "HALT" infinite times
+// This is a pretty unsafe function thats used for some debugging
+// NEVER CALL THIS UNLESS YOU ABSOLUTELY NEED TO
+void hcf(void) {
+    for (;;) {
+        asm("hlt");
+    }
+}
 
 void framebuffer_init(void)
 {
+    if (!LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision)) {
+        serial_printf("error: unsupported Limine base revision");
+        hcf();
+    }
+
+    if (framebuffer_request.response == NULL ||
+        framebuffer_request.response->framebuffer_count < 1) {
+        serial_printf("error: framebuffer request failed");
+        hcf();
+    }
+
+    struct limine_framebuffer* fb = framebuffer_request.response->framebuffers[0];
+
+    for (size_t i = 0; i < 100; i++) {
+        volatile uint32_t* fb_ptr = fb->address;
+        fb_ptr[i * (fb->pitch / 4) + i] = 0xffffff;
+    }
 }
 
 void fb_put_pixel(int x, int y, uint32_t color)
