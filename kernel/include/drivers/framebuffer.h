244fbc694d7663a73b9f001d070196cff78d5f77
suak7 <celestinedior1225@gmail.com>
2025-12-13 10:36:47 -0500

    add bg color param to fb_draw_char/fb_draw_string (#17)

--- kernel/include/drivers/framebuffer.h
+++ kernel/include/drivers/framebuffer.h
@@ -16,7 +16,7 @@ void framebuffer_init(void);
 void fb_put_pixel(uint32_t x, uint32_t y, uint32_t color);
 void fb_clear(uint32_t color);
 void fb_fill_rect(uint64_t x, uint64_t y, uint64_t width, uint64_t height, uint32_t color);
-void fb_draw_char(char c, int x, int y, uint32_t color, int scale);
-void fb_draw_string(const char* str, int x, int y, uint32_t color, int scale);
+void fb_draw_char(char c, int x, int y, uint32_t fg_color, uint32_t bg_color, int scale);
+void fb_draw_string(const char* str, int x, int y, uint32_t fg_color, uint32_t bg_color, int scale);
 
 #endif
