def2314fa8585f61a094d17336620265a3a359bb
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-15 16:35:41 -0500

    add gdt and tss header

---
+++ kernel/include/cpu/gdt.h
@@ -0,0 +1,32 @@
+#ifndef GDT_H
+#define GDT_H
+
+#include <stdint.h>
+
+typedef union gdt_entry 
+{
+    struct {
+        uint16_t limit;
+        uint16_t base_low;
+        uint8_t  base_mid;
+        uint8_t  access_byte;
+        uint8_t  flags;
+        uint8_t  base_high;
+    };
+    struct {
+        uint32_t base_upper;
+        uint32_t reserved;
+    };
+
+} __attribute__((packed)) gdt_entry_tss;
+
+
+typedef struct 
+{
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed)) gdt_register;
+
+void init_gdt();
+
+#endif
