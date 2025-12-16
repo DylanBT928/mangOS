2f73b53241ef5f630ee938c635f4fc40ed0e0630
Dylan Tran <dylanbt928@gmail.com>
2025-12-15 16:44:11 -0800

    remove unused includes

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -2,11 +2,9 @@
 #define KERNEL_H
 
 #include "colors.h"
-#include "drivers/framebuffer.h"
 #include "drivers/serial.h"
 #include "drivers/terminal.h"
 #include "libc/stdlib.h"
-#include "libc/string.h"
 
 void hcf(void);
 
