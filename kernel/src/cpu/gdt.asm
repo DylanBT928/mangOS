9e7c057388a24552642434057905d20aacf847ac
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-28 14:15:36 -0500

    modify gdt reloading logic

--- kernel/src/cpu/gdt.asm
+++ kernel/src/cpu/gdt.asm
@@ -6,6 +6,14 @@ load_gdt:
     ; load the GDT register
     lgdt [rdi]
 
+reload_cs:
+    ; reload code segment
+    push 0x08
+    lea rax, [rel .reload_ds]
+    push rax
+    retfq
+
+.reload_ds:
     ; reload data segment registers
     mov ax, 0x10
     mov ds, ax
@@ -13,14 +21,7 @@ load_gdt:
     mov fs, ax
     mov gs, ax
     mov ss, ax
-
-    ; reload code segment
-    pop rdi
-    mov rax, 0x08
-    push rax
-    push rdi
-    db 0x48
-    retf
+    ret
 
 load_tss:
     mov ax, 0x28
